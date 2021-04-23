5db4e05439da8d7984e3ba6aa533993d3690afc2
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-04-23 14:44:50 -0300

    It works

--- replacePxToRem.js
+++ replacePxToRem.js
@@ -1,63 +1,58 @@
-//import { opendir } from 'fs/promises';
 const { opendir } = require('fs/promises')
 const fs = require('fs')
 const PATH = require('path')
 
+// don't check all directorys in this array is case sensitive
+const dontPass = ['dontpass', 'dontpass2', 'nodemodules']
+
+// RegExp obj for filtler
 const REGEX = {
-  cssExt : /.*.css/,
+  cssExt : /[^2].css$/,
   withExt : /.*\..*/,
-  unitPx: /.*px/
+  unitPx: /.*px/,
+  newExt: /.{4}$/
 }
 
-const escritor = async(path) => {
+// checks for files
+const isCssFile = name => REGEX.cssExt.test(name)
+const withoutAccess = name => !dontPass.some(dir => dir === name)
+const dontHasExt = name => !REGEX.withExt.test(name)
+const isDir = name => withoutAccess(name) && dontHasExt(name)
+const doSelfHasPx = name => REGEX.unitPx.test(name)
+const changeLine = text => doSelfHasPx(text)
+  ? text.replace(/[0-9]*px/g, L => L.replace(/px/, '')/ 16 +'rem')
+  : text
 
-  const read = fs.createReadStream(PATH.join(__dirname, path), {encoding: 'utf-8'} )
+
+const changePxToRem = async(path) => {
+  const newCss = path.replace(REGEX.newExt, '2.css')
+  const read = fs.createReadStream(path, {encoding: 'utf-8'} )
+  const write = fs.createWriteStream(newCss)
 
   read.on('data', chuck => {
-
     lines = chuck.split('\n')
-    for(const line of lines){
-      if(REGEX.unitPx.test(line)) {
-        const rem = line.replace(/.{1,3}px/g, L =>
-          ' ' + L.replace(/px/, '')/ 16 +'rem'
-        )
-        console.log(line, '<- before')
-        console.log(rem, '<- after')
-      }
-    }
 
+    for(let line of lines) {
+      line = changeLine(line)
+      write.write(line + '\n')
+    }
   })
 }
 
-// escritor('./example/main.css')
-
-
-const dontPass = ['dontpass']
-
-const searchFile = async path => {
-  try {
-    const directory = await opendir(path);
-    for await(const { name } of directory) {
-      switch(true) {
-        case REGEX.cssExt.test(name):
-          console.log(`I\'m a css file ---> `,name)
-          break
-        case REGEX.withExt.test(name):
-          console.log(`I\'m not a css file --->`, name)
-          break
-        case dontPass.some( dir => dir ===name):
-          console.log(`Sorry, don't have allow for this directory ---->`, name)
-          break
-        default:
-            console.log('Maybe i\'m a directory --->', name)
-            searchFile(`${path}/${name}`)
-          break
-      }
-    }
-  }
-  catch(err){
-    console.log('I\'m not a directory', path.replace(/.*\//, ''))
+const action = (nameFile, path) => {
+  if(isDir(nameFile)){
+    searchFile(PATH.join(path, nameFile))
+  } else if(isCssFile(nameFile)){
+    changePxToRem(PATH.join(path, nameFile))
   }
 }
 
-searchFile('./')
+const searchFile = async path => {
+  try {
+    const directory = await opendir(path)
+    for await(const { name } of directory) action(name, path)
+  }
+  catch(err) {} // this error is if the file isn't directory
+}
+
+searchFile(__dirname)
